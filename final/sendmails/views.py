1de78002e66de6cfe8674084465df40067917c33
Adheesha Navodi Ranathunga <adheeshanavodi@gmail.com>
2020-05-14 12:49:47 +0530

    first commit

--- final/sendmails/views.py
+++ final/sendmails/views.py
@@ -5,9 +5,10 @@ from django.core.mail import send_mail
 
 # Create your views here.
 def mail(request):
-    subject = "Test mail"
-    msg = "Congratulations ,Email is done"
+    subject = "Prediction"
+    msg = "Prediction in 2020 shows high at the begining of the year and gradually it dicreases the number of dengue cases in Colombo"
     to = "adheeshanavodi@gmail.com"
+
     res= send_mail(subject,msg, settings.EMAIL_HOST_USER, [to])
 
     if (res == 1):
